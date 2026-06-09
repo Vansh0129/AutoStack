5db030bdb262ab09f8dd505e661e8f5450188c6e
Vansh lute <vanshlute@gmail.com>
2026-06-10 02:00:24 +0530

    Basic Project file updated

---
+++ AutoStack/src/main/java/com/projectFile/AutoStack/AutoStackApplication.java
@@ -0,0 +1,13 @@
+package com.projectFile.AutoStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoStackApplication.class, args);
+	}
+
+}
---
+++ AutoStack/src/test/java/com/projectFile/AutoStack/AutoStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectFile.AutoStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
